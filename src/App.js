704806f665a5438b63b7f8bf6a55211535e42b68
yangfei <yf170481206>
2019-01-25 12:10:08 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome to React</p>
+        </header>
       </div>
     );
   }
