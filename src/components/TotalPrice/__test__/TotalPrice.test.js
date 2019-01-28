03805191d4fe811cf1ae91a51cdaccb6cc35b905
yangfei <yf170481206>
2019-01-29 07:53:53 +0800

    完成测试

--- src/components/TotalPrice/__test__/TotalPrice.test.js
+++ src/components/TotalPrice/__test__/TotalPrice.test.js
@@ -14,6 +14,7 @@ describe('测试价格汇总组件', () => {
     wrapper =  shallow(<TotalPrice {...props} />);
   });
   it('匹配组件快照', () => {
+    // 保证组件DOM和上次一致，如果要更新可以使用 -u 参数更新组件
     expect(wrapper).toMatchSnapshot()
   });
   it('测试收入金额和支出金额', () => {
