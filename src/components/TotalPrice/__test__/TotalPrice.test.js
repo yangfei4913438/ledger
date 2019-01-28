17628eb24f336830bbd6f19989555e748da1e331
yangfei <yf170481206>
2019-01-29 07:37:25 +0800

    完成测试

--- src/components/TotalPrice/__test__/TotalPrice.test.js
+++ src/components/TotalPrice/__test__/TotalPrice.test.js
@@ -7,9 +7,16 @@ const props = {
   total_expense: 800
 };
 
-describe('test TotalPrice component', () => {
-  it('test total_income && total_expense', () => {
-    const wrapper = shallow(<TotalPrice {...props} />);
+let wrapper;
+describe('测试价格汇总组件', () => {
+  // 每次测试前都会执行
+  beforeEach(() => {
+    wrapper =  shallow(<TotalPrice {...props} />);
+  });
+  it('匹配组件快照', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+  it('测试收入金额和支出金额', () => {
     expect(wrapper.find('#income').text() * 1).toEqual(1000);
     expect(wrapper.find('#expense').text() * 1).toEqual(800);
   })
